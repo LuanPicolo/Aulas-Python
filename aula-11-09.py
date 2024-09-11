dab6f7eed7f6538aff3fc405ad4623a8171e9a08
LuanPicolo <161030989+LuanPicolo@users.noreply.github.com>
2024-09-11 11:13:36 -0300

    Create aula-11-09.py

---
+++ aula-11-09.py
@@ -0,0 +1,80 @@
+'''
+print("Tabuada do 2")
+
+p = 0
+
+while p < 20:
+    p = p+2
+    print(p)
+
+print("Tabuada do 5")
+
+x = 0
+
+while x < 50:
+    x = x+5
+    print(x)
+
+print("Tabuada do 7")
+
+y = 0
+
+while y < 70:
+    y = y+7
+    print(y)
+
+
+
+texto = input("Insira algo aqui: ")
+
+for x, i in enumerate(texto, 1):
+    numero = x
+
+print("Esse texto possui", numero, "caracteres.")
+'''
+
+'''
+p1 = "Curso"
+p2 = "Python"
+contador = 0
+
+while contador < 10:
+    contador = contador+1
+    print(p1, "\n")
+    print(p2, "\n")
+'''
+
+
+'''
+nota = int(input("Digite sua nota: "))
+
+if nota in range(0, 10):
+    print("A sua nota é valida! (", nota, ")")
+else:
+    print("A sua nota é invalida! (", nota, ")")
+'''
+
+
+idadeNovo = 3000
+idadeVelho = 0
+print("Para finalizar a execução, digite um numero negativo")
+idade = int(input("Digite a idade: "))
+
+while idade > 0:
+    if idade < idadeNovo: # surgiu uma pessoa mais nova
+        idadeNovo = idade
+    if idade > idadeVelho:
+        idadeVelho = idade
+    if idade < 0:
+        break
+print("Nova", idadeNovo)
+print("Velho", idadeVelho)
+'''
+
+print("Para terminar a leitura, digite um numero negativo", "\n")
+idade = int(input("Digite a idade: "))
+idadeNova =
+idadeVelha = 
+
+while idade < 0:
+'''
